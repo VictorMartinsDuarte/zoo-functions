5ddf4de941db6a385deeeb9870f9e2fcf82d0c63
Victor Martins <victor.martins.duarte@gmail.com>
2021-08-04 16:37:23 -0300

    Requirement 6

--- src/zoo.js
+++ src/zoo.js
@@ -1,15 +1,16 @@
 const data = require('./data');
 const { species, employees } = require('./data');
 
+// *-- 1 --*
 function getSpeciesByIds(...ids) {
   return species.filter((element, index) => element.id === ids[index]);
 }
-
+// *-- 2 --*
 function getAnimalsOlderThan(animal, age) {
   const animalObject = species.find((obj) => obj.name === animal);
   return animalObject.residents.every((element) => element.age >= age);
 }
-
+// *-- 3 --*
 function getEmployeeByName(employeeName) {
   return employees.reduce((acc, current) => {
     if (current.firstName === employeeName || current.lastName === employeeName) {
@@ -18,11 +19,11 @@ function getEmployeeByName(employeeName) {
     return acc;
   }, {});
 }
-
+// *-- 4 --*
 function createEmployee(personalInfo, associatedWith) {
   return { ...personalInfo, ...associatedWith };
 }
-
+// *-- 5 --*
 function isManager(id) {
   const managersId = employees.map(({ managers }) => managers).reduce((acc, current) => {
     acc.push(...current);
@@ -34,35 +35,45 @@ function isManager(id) {
 
   return managersFiltered.some((element) => element === id);
 }
+// *-- 6 --*
+function addEmployee(id, firstName, lastName, managers = [], responsibleFor = []) {
+  if (!id || !firstName || !lastName) return 'Preencha os parâmetros';
 
-function addEmployee(id, firstName, lastName, managers, responsibleFor) {
-  // seu código aqui
+  const newEmployee = {
+    id,
+    firstName,
+    lastName,
+    managers,
+    responsibleFor,
+  };
+
+  return employees.push(newEmployee);
 }
-
+// *-- 7 --*
 function countAnimals(speciesName) {
   // seu código aqui
 }
-
+// *-- 8 --*
 function calculateEntry(entrants) {
   // seu código aqui
 }
-
+// *-- 9 --*
 function getAnimalMap(options) {
   // seu código aqui
 }
-
+// *-- 10 --*
 function getSchedule(dayName) {
   // seu código aqui
 }
-
+// *-- 11 --*
 function getOldestFromFirstSpecies(id) {
   // seu código aqui
 }
-
+// *-- 12 --*
 function increasePrices(percentage) {
   // seu código aqui
 }
-
+// *-- 13 --*
 function getEmployeeCoverage(idOrName) {
   // seu código aqui
 }
